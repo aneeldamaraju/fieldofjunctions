30bd68dc5939e7fd2cbca0ff60a58c9c2c14b3b3
aneeldamaraju <aneeldamaraju@gmail.com>
2022-01-03 17:21:32 -0500

    CAVIbase

---
+++ patch_CAVI_unary.py
@@ -0,0 +1,137 @@
+import torch
+import numpy as np
+
+perm_list = torch.tensor([[0, 1, 2],
+                          [0, 2, 1],
+                          [1, 2, 0],
+                          [1, 0, 2],
+                          [2, 0, 1],
+                          [2, 1, 0]]).long()
+
+inv_perm_list = torch.tensor([[0, 1, 2],
+                              [0, 2, 1],
+                              [2, 0, 1],
+                              [1, 0, 2],
+                              [1, 2, 0],
+                              [2, 1, 0]]).long()
+
+
+def get_permutations(region_patches):
+    """
+    region_patches is [1, 1, 3, 1, RF, RF, H_patches, W_patches
+    """
+    return region_patches[0, 0, perm_list.permute(1, 0), 0, :, :, :, :]  # [3, 6, RF, RF, H_patches, W_patches]
+
+
+def resolve_permutations_mrf(permutations, stride, neighborhood=4, max_iters=None, func=None, func2=None, seed=None):
+    """
+    #region_patches has shape [1, 1, 3, 1, RF, RF, H_patches, W_patches]
+    permutations has shape [3, 6, RF, RF, H_patches, W_patches]
+    """
+
+    assert neighborhood in [4, 8], "neighborhood must be 4 or 8"
+
+    H_patches = permutations.shape[-2]
+    W_patches = permutations.shape[-1]
+
+    RF = permutations.shape[-3]
+    assert permutations.shape[-4] == RF, "RF must be square"
+
+    if func is None:
+        func = lambda x: 10.0 * torch.exp(-100.0 * x)
+
+    if func2 is None:
+        func2 = lambda x: 10.0 * torch.exp(-100.0 * x)
+
+    if max_iters is None:
+        max_iters = 2 * max([W_patches, H_patches])
+
+    # Create a 6x6xHx(W-1) array of log pairwise potentials for horizontal edges
+    # and a 6x6x(H-1)xW array for vertical ones.
+
+    # horizontal_logpsi[k, l, i, j] is the log psi function between the (i, j) and (i, j+1) patches evaluated at
+    # the kth and lth permutation respectively.
+    horizontal_logpsi = func((permutations[:, :, :, stride:, :, :-1].unsqueeze(2) -
+                              permutations[:, :, :, :-stride, :, 1:].unsqueeze(1)).abs().sum(0).mean(2).mean(2))
+
+    # vertical_logpsi[k, l, i, j] is the log psi function between the (i, j) and (i+1, j) patches evaluated at
+    # the kth and lth permutation respectively.
+    vertical_logpsi = func((permutations[:, :, stride:, :, :-1, :].unsqueeze(2) -
+                            permutations[:, :, :-stride, :, 1:, :].unsqueeze(1)).abs().sum(0).mean(2).mean(2))
+
+    # Add padding to avoid having to deal with image boundaries
+    horizontal_logpsi = torch.nn.functional.pad(horizontal_logpsi, pad=(1, 1, 1, 1))
+    vertical_logpsi = torch.nn.functional.pad(vertical_logpsi, pad=(1, 1, 1, 1))
+
+    if neighborhood == 8:
+        # angle45_logpsi[k, l, i, j] is the log psi function between the (i, j) and (i+1, j+1) patches evaluated at
+        # the kth and lth permutations respectively.
+        angle45_logpsi = func2((permutations[:, :, stride:, stride:, :-1, :-1].unsqueeze(2) -
+                                permutations[:, :, :-stride, :-stride, 1:, 1:].unsqueeze(1)).abs().sum(0).mean(2).mean(
+            2))
+
+        # angle135_logpsi[k, l, i, j] is the log psi function between the (i, j+1) and (i+1, j) patches evaluated at
+        # the kth and lth permutations respectively. [Note: This is a shifted version of the match between (i, j) and (i+1, j-1).]
+        angle135_logpsi = func2((permutations[:, :, stride:, :-stride, :-1, 1:].unsqueeze(2) -
+                                 permutations[:, :, :-stride, stride:, 1:, :-1].unsqueeze(1)).abs().sum(0).mean(2).mean(
+            2))
+
+        # Add padding to avoid having to deal with image boundaries
+        angle45_logpsi = torch.nn.functional.pad(angle45_logpsi, pad=(1, 1, 1, 1))
+        angle135_logpsi = torch.nn.functional.pad(angle135_logpsi, pad=(1, 1, 1, 1))
+
+    # Set all distributions to uniform except seed which is set to [1, 0, 0, 0, 0, 0].
+    lmbdas = torch.ones((6, H_patches + 2, W_patches + 2), dtype=torch.float32).to(permutations.device) / 6.0
+
+    # Unless specified, set seed to the non-constant patch closest to center
+    if seed is None:
+        # Find patches with at least 2 colors (i.e. no uniform region)
+        good_patches = permutations[:, 0, :, :, :, :].max(dim=-3)[0].max(dim=-3)[0].sum(0) > 1.5
+
+        # Find closest such patch to the center
+        good_patch_inds = good_patches.nonzero()
+        if len(good_patch_inds.shape) < 2:
+            seed = (H_patches // 2, W_patches // 2)
+        else:
+            gpiy = good_patch_inds[:, 0] - (H_patches - 1) / 2.0
+            gpix = good_patch_inds[:, 1] - (W_patches - 1) / 2.0
+            ind = torch.argmin(gpix ** 2 + gpiy ** 2)
+
+            seed = good_patch_inds[ind, :]
+
+    lmbdas[0, seed[0] + 1, seed[1] + 1] = 1.0
+    lmbdas[1:, seed[0] + 1, seed[1] + 1] = 0.0
+    for t in range(max_iters):
+
+        # Left neighbor:   sum over permutations of the (i, j-1)th patch, i.e. dimension 0
+        s = (lmbdas.unsqueeze(1)[:, :, 1:-1, :-2] * horizontal_logpsi[:, :, 1:-1, :-1]).sum(dim=0)
+        # Right neighbor:  sum over permutations of the (i, j+1)th patch, i.e. dimension 1
+        s += (lmbdas.unsqueeze(0)[:, :, 1:-1, 2:] * horizontal_logpsi[:, :, 1:-1, 1:]).sum(dim=1)
+        # Top neighbor:    sum over permutations of the (i-1, j)th patch, i.e. dimension 0
+        s += (lmbdas.unsqueeze(1)[:, :, :-2, 1:-1] * vertical_logpsi[:, :, :-1, 1:-1]).sum(dim=0)
+        # Bottom neighbor: sum over permutations of the (i+1, j)th patch, i.e. dimension 1
+        s += (lmbdas.unsqueeze(0)[:, :, 2:, 1:-1] * vertical_logpsi[:, :, 1:, 1:-1]).sum(dim=1)
+
+        if neighborhood == 8:
+            # TL neighbor: sum over permutations of the (i-1, j-1)th patch, i.e. dimension 0
+            s += (lmbdas.unsqueeze(1)[:, :, :-2, :-2] * angle45_logpsi[:, :, :-1, :-1]).sum(dim=0)
+            # BR neighbor: sum over permutations of the (i+1, j+1)th patch, i.e. dimension 1
+            s += (lmbdas.unsqueeze(0)[:, :, 2:, 2:] * angle45_logpsi[:, :, 1:, 1:]).sum(dim=1)
+            # TR neighbor: sum over permutations of the (i-1, j+1)th patch, i.e. dimension 0
+            s += (lmbdas.unsqueeze(1)[:, :, :-2, 2:] * angle135_logpsi[:, :, :-1, 1:]).sum(dim=0)
+            # BL neighbor: sum over permutations of the (i+1, j-1)th patch, i.e. dimension 1
+            s += (lmbdas.unsqueeze(0)[:, :, 2:, :-2] * angle135_logpsi[:, :, 1:, :-1]).sum(dim=1)
+
+        # Update parameters according to CAVI update
+        lmbdas[:, 1:-1, 1:-1] = torch.exp(s)
+
+        # Normalize and reset top-left distribution
+        lmbdas /= lmbdas.sum(dim=0, keepdim=True)
+        lmbdas[0, seed[0] + 1, seed[1] + 1] = 1.0
+        lmbdas[1:, seed[0] + 1, seed[1] + 1] = 0.0
+
+    lmbdas = lmbdas[:, 1:-1, 1:-1]
+
+    indices = lmbdas.argmax(0)  # shape [H_patches, W_patches]
+
+    return indices
